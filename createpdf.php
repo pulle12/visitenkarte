c33e9eface03e73c62b6237d2ca2aecfb8449a6c
piusrauch <pirauch@tsn.at>
2026-02-06 09:23:25 +0100

    Versuch in zelle vorname und nachname auszugeben

--- createpdf.php
+++ createpdf.php
@@ -1,8 +1,9 @@
 <?php
 require_once("vendor/autoload.php");
-
+require_once("index.php");
 $pdf = new FPDF();
 $pdf->AddPage();
 $pdf->SetFont("Arial", "B", 16);
-$pdf->Cell(40, 10, "Hello, World!");
+$pdf->Cell(40, 10, Visitenkarte.getVorname() . " " . Visitenkarte::getNachname());
+$pdf->Cell(40, 10, "");
 $pdf->Output();
